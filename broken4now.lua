64666224e6dba200e0e911b04df7b17d6654f418
moneymanlol <gussybetter@gmail.com>
2026-06-22 06:24:08 -0400

    Update broken4now.lua

--- broken4now.lua
+++ broken4now.lua
@@ -1,14 +1,13 @@
-
 local RootPart = workspace.furry34400.HumanoidRootPart
 local Toys = workspace.furry34400SpawnedInToys
 local CreatureRootPart = workspace.furry34400SpawnedInToys.CreatureBlobman.HumanoidRootPart
-[RootPart]CFrame = CFrame.new(0, 0, 10)
-[CreatureRootPart]CFrame = CFrame.new(workspace[Toys]YouDecoy.HumanoidRootPart)
+[RootPart]CFrame = CFrame.new(0, 10, 10)
+[CreatureRootPart]CFrame = CFrame.new(0, 10, 10)
 -- now here's the good stuff (if im good at coding)
 while true do
 
 task.wait(0.05)
--- btw the CFrames are just for testing ts out, RootPart tp'ing MAY not be needed at all, it's either that or CreatureBlobman.HumanoidRootPart teleportation.
+-- btw the CFrames are just for testing ts out, RootPart RootPart tp'ing MAY not be needed at all, it's either that or CreatureBlobman.HumanoidRootPart teleportation.
   -- also i might need to look inside the game with dex
   -- papyrus would be proud of my commentation,
   -- peak music btw
@@ -21,6 +20,9 @@ Event:FireServer(
 ) local CreatureDrop = workspace[Toys]CreatureBlobman.BlobmanSeatAndOwnerScript.CreatureDrop
 Event:FireServer(
     workspace[Toys]CreatureBlobman.LeftDetector.LeftWeld
-  ) CreatureGrab
-end
--- im bad at coding -_-
+  ) local CreatureGrab = workspace[Toys]CreatureBlobman.BlobmanSeatAndOwnerScript.CreatureGrab
+Event:FireServer(
+    workspace[Toys]CreatureBlobman.LeftDetector,
+    workspace.[Toys]YouDecoy.HumanoidRootPart,
+    workspace[Toys]CreatureBlobman.LeftDetector.LeftWeld
+) end
